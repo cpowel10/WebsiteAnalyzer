29d3976ee69209d2a75b17b54cd253c18a9d26e0
cpo <cpowe041@odu.edu>
2020-07-14 21:18:34 -0400

    HTMLDocument done, all tests passing

--- src/main/java/edu/odu/cs/cs350/HTMLDocument.java
+++ src/main/java/edu/odu/cs/cs350/HTMLDocument.java
@@ -1,17 +1,68 @@
 package edu.odu.cs.cs350;
 
-import java.util.Collection;
+import java.nio.file.Path;
+import java.util.LinkedList;
 
 public class HTMLDocument {
-  	private Collection<Image> allImages;
-  	private Collection<Style> allStyles;
-  	private Collection<Script> allScripts;
-  	private Collection<Anchor> allAnchors;
+  	private LinkedList<Image> allImages;
+  	private LinkedList<Style> allStyles;
+  	private LinkedList<Script> allScripts;
+  	private LinkedList<Anchor> allAnchors;
   	private int totalImageSize;
-  	private String path;
+  	private Path path;
   	
-	public HTMLDocument() {
-		// TODO Auto-generated constructor stub
+  	/*
+  	 * set private path variable to given userPath
+  	 */
+	public HTMLDocument(Path userPath) {
+		this.path = userPath;
+		allImages = new LinkedList<Image>();
+		allStyles = new LinkedList<Style>();
+		allScripts = new LinkedList<Script>();
+		allAnchors = new LinkedList<Anchor>();
+		totalImageSize = 0;
 	}
-
+	
+	/*
+	 * adds sizes of all Image objects in
+	 * allImages and returns the totalImageSize
+	 * for the current HTMLDocument
+	 */
+	public int getTotalImageSize() {
+		return totalImageSize;
+	}
+	
+	public Path getPath() {
+		return path;
+	}
+	
+	public void addElement(Object tag) {
+		if (tag instanceof Anchor) {
+			allAnchors.add((Anchor) tag);
+		}
+		else if (tag instanceof Style) {
+			allStyles.add((Style) tag);
+		}
+		else if (tag instanceof Script) {
+			allScripts.add((Script) tag);
+		}
+		else {
+			allImages.add((Image) tag);
+			totalImageSize += ((Image) tag).size();
+		}
+	}
+	
+	/*
+	 * For Testing Only Below
+	 * <path> <num_img> <num_style> <num_script> <num_anchor> <totalImageSize>
+	 */
+	public String toString() {
+		String str = path.toString() + " ";
+		str += Integer.toString(allImages.size()) + " ";
+		str += Integer.toString(allStyles.size()) + " ";
+		str += Integer.toString(allScripts.size()) + " ";
+		str += Integer.toString(allAnchors.size()) + " ";
+		str += Integer.toString(totalImageSize);
+		return str;
+	}
 }
---
+++ src/test/java/edu/odu/cs/cs350/TestHTMLDocument.java
@@ -0,0 +1,80 @@
+package edu.odu.cs.cs350;
+
+import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertThat;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+import edu.odu.cs.cs350.Enum.Externality;
+import edu.odu.cs.cs350.Enum.TagType;
+
+import static org.hamcrest.CoreMatchers.*;
+
+public class TestHTMLDocument {
+
+	String fileName = "/c/Users/chris/Downloads/";
+	Path path1 = Paths.get(fileName);
+	LinkedList<Path> llp = new LinkedList<Path>();
+	LinkedList<Path> llp2 = new LinkedList<Path>();
+
+	Image img1 = new Image(TagType.IMAGE, 2, llp, 1, llp2);
+	Image img2 = new Image(TagType.IMAGE, 3, llp, 1, llp2);
+	Image img3 = new Image(TagType.IMAGE, 13, llp, 1, llp2);
+	Style sty1 = new Style("fake/path", Externality.INTERNAL);
+	Style sty2 = new Style("fake/path2", Externality.EXTERNAL);
+	Script script1 = new Script("fake/path", Externality.INTRA);
+	Anchor anch1 = new Anchor("fake/path", Externality.INTERNAL);
+	HTMLDocument page1 = new HTMLDocument(path1);
+
+	@Test
+	public void testConstructorEmpty() {
+		assertThat(page1.getPath(), is(path1));
+		assertThat(page1.toString(), is("\\c\\Users\\chris\\Downloads 0 0 0 0 0"));
+	}
+	
+	@Test
+	public void testAddImage() {
+		page1.addElement(img1);
+		assertThat(page1.getPath(), is(path1));
+		assertThat(page1.toString(), is("\\c\\Users\\chris\\Downloads 1 0 0 0 2"));
+	}
+	
+	@Test
+	public void testAddScript() {
+		page1.addElement(script1);
+		assertThat(page1.getPath(), is(path1));
+		assertThat(page1.toString(), is("\\c\\Users\\chris\\Downloads 0 0 1 0 0"));
+	}
+	
+	@Test
+	public void testAddStyle() {
+		page1.addElement(sty1);
+		assertThat(page1.getPath(), is(path1));
+		assertThat(page1.toString(), is("\\c\\Users\\chris\\Downloads 0 1 0 0 0"));
+	}
+
+	@Test
+	public void testAddAnchor() {
+		page1.addElement(anch1);
+		assertThat(page1.getPath(), is(path1));
+		assertThat(page1.toString(), is("\\c\\Users\\chris\\Downloads 0 0 0 1 0"));
+	}
+	
+	@Test
+	public void testAddAll() {
+		page1.addElement(img1);
+		page1.addElement(img2);
+		page1.addElement(img3);
+		page1.addElement(script1);
+		page1.addElement(sty1);
+		page1.addElement(sty2);
+		page1.addElement(anch1);
+		assertThat(page1.getPath(), is(path1));
+		assertThat(page1.toString(), is("\\c\\Users\\chris\\Downloads 3 2 1 1 18"));
+	}
+}
