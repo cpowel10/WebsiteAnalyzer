1a361cb8b4a983c19417bcf39bf7e6a8e7f31f6e
kgrissom <kgris001@odu.edu>
2020-07-20 17:06:00 -0400

    Skeleton Tag with hopefully completed TestTag

--- src/main/java/edu/odu/cs/cs350/Tag.java
+++ src/main/java/edu/odu/cs/cs350/Tag.java
@@ -1,15 +1,40 @@
 package edu.odu.cs.cs350;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import edu.odu.cs.cs350.Enum.Externality;
 import edu.odu.cs.cs350.Enum.TagType;
 
 public class Tag {
 
-	protected String path;
-	protected TagType type;
-	protected Externality eality;
+	private Path path;
+	private TagType type;
+	private Externality externality;
 	
-//	public Tag() {
-//		
-//	}
+
+	public Tag() {
+		this.externality = Externality.EXTERNAL;
+	}
+	public Tag(Path pth, TagType typ, Externality ext) {
+		this.externality = Externality.EXTERNAL;
+	}
+	public Path path() {
+		return Paths.get("null");
+	}
+	public TagType type() {
+		return TagType.ANCHOR;
+	}
+	public Externality externality() {
+		return Externality.EXTERNAL;
+	}
+	public void setPath(Path path) {
+		
+	}
+	public void setType(TagType type) {
+		
+	}
+	public void setExternality(Externality ext) {
+		
+	}
 }
