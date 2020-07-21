0a26673c7b259161a4f04a58384f0a28f529d241
rpeabody <rpeabody@cs.odu.edu>
2020-07-21 15:53:33 -0400

    needed to import path

--- src/main/java/edu/odu/cs/cs350/Anchor.java
+++ src/main/java/edu/odu/cs/cs350/Anchor.java
@@ -3,8 +3,10 @@ package edu.odu.cs.cs350;
 import edu.odu.cs.cs350.Enum.Externality;
 import edu.odu.cs.cs350.Enum.TagType;
 
+import java.nio.file.Path;
+
 public class Anchor extends Tag {	
-	public Anchor(String inPath, Externality inEality) {
+	public Anchor(Path inPath, Externality inEality) {
 		super.path = inPath;
 		super.type = TagType.ANCHOR;
 		super.eality = inEality;
