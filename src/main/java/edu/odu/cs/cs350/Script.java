3152246d99bcac23ff2d9ed7d94b1869ce0a0ce5
rpeabody <rpeabody@cs.odu.edu>
2020-07-21 16:34:25 -0400

    Path import

--- src/main/java/edu/odu/cs/cs350/Script.java
+++ src/main/java/edu/odu/cs/cs350/Script.java
@@ -2,9 +2,10 @@ package edu.odu.cs.cs350;
 
 import edu.odu.cs.cs350.Enum.Externality;
 import edu.odu.cs.cs350.Enum.TagType;
+import java.nio.file.Path;
 
 public class Script extends Tag {	
-	public Script(String inPath, Externality inEality) {
+	public Script(Path inPath, Externality inEality) {
 		super.path = inPath;
 		super.type = TagType.SCRIPT;
 		super.eality = inEality;
--- src/main/java/edu/odu/cs/cs350/Style.java
+++ src/main/java/edu/odu/cs/cs350/Style.java
@@ -2,9 +2,10 @@ package edu.odu.cs.cs350;
 
 import edu.odu.cs.cs350.Enum.Externality;
 import edu.odu.cs.cs350.Enum.TagType;
+import java.nio.file.Path;
 
 public class Style extends Tag {
-	public Style(String inPath, Externality inEality) {
+	public Style(Path inPath, Externality inEality) {
 		// TODO Auto-generated constructor stub
 		super.path = inPath;
 		super.type = TagType.STYLE;
