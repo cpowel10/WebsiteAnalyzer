8c183f9c6e09280da6f35126de19f8e3628d5639
kgrissom <kgris001@odu.edu>
2020-07-23 22:18:25 -0400

    Comments for non-selfDocumenting part

--- src/main/java/edu/odu/cs/cs350/HTMLDocument.java
+++ src/main/java/edu/odu/cs/cs350/HTMLDocument.java
@@ -109,6 +109,7 @@ public class HTMLDocument implements Comparable<HTMLDocument> {
 			totalImageSize += ((Image) tag).size();
 		}
 	}
+	//Override for comparison, used in GenerateReport for Collections.sort(List)
 	@Override
 	public int compareTo(HTMLDocument doc) {
 		
@@ -116,18 +117,22 @@ public class HTMLDocument implements Comparable<HTMLDocument> {
 		String rhsPath = (doc.getPath()).toString(); 
 		int lhsLength = lhsPath.length();
 		int rhsLength = rhsPath.length();
+		
+		//grab a quick max length for iteration/indexing
 		int max = 0;
 		if(lhsLength > rhsLength)
 			max = lhsLength;
 		else
 			max = rhsLength;
+		
+		//go over every character  of the path to compare
 		for(int i = 0; i < max; i++) {
 			if((int)lhsPath.charAt(i) - (int)rhsPath.charAt(i) > 0)
 				return 1; //difference is greater than 0 means lhs is larger(comes later in alphabet)
 			else if((int)lhsPath.charAt(i) - (int)rhsPath.charAt(i) < 0)
-				return -1;
+				return -1; //lhs is smaller(comes earlier in alphabet)
 		}
-		return 0; 
+		return 0; //equal 
 	}
  
 	
