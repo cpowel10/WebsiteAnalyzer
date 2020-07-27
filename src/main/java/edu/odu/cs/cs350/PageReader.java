f9de682131c612eab36c3a41d0d4ba9af59781ff
cpo <cpowe041@odu.edu>
2020-07-27 13:10:00 -0400

    scanForScripts and scanForStyles written

--- src/main/java/edu/odu/cs/cs350/PageReader.java
+++ src/main/java/edu/odu/cs/cs350/PageReader.java
@@ -11,6 +11,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import edu.odu.cs.cs350.Enum.Externality;
 import edu.odu.cs.cs350.Enum.TagType;
 
 public class PageReader {
@@ -59,23 +60,61 @@ public class PageReader {
 	/*
 	* 
 	*/
-	public LinkedList<Style> scanForStyles(Path path){
-		return null;
+	public LinkedList<Style> scanForStyles(Path path) throws IOException{
+		LinkedList<Path> pathList = new LinkedList<Path>();
+		LinkedList<Style> styleList = new LinkedList<Style>();
+		pathList.add(path);
+		//use jsoup library to find image tags
+		File pathFile = path.toFile();
+		Document doc = Jsoup.parse(pathFile, "UTF-8");
+		//String in select() will be a String to identify 
+		//what the method will search for and add to the Elements(list)
+		Elements links = doc.getElementsByTag("link");
+		//add each script file found in File to LinkedList<Script> 
+		//in given HTMLDocument Object ignoring inline scripts
+		for (Element s : links) {
+			if (s.attr("rel") == "stylesheet") {
+				Style tempStyle = new Style();
+				Path stylePath = Paths.get(s.attr("href"));
+				tempStyle.setPath(stylePath);
+				styleList.add(tempStyle);
+			}
+		}
+		return styleList;
 		
 	}
 	
 	/*
 	* 
 	*/
-	public LinkedList<Script> scanForScripts(Path path){
-		return null;
+	public LinkedList<Script> scanForScripts(Path path) throws IOException{
+		LinkedList<Path> pathList = new LinkedList<Path>();
+		LinkedList<Script> scriptList = new LinkedList<Script>();
+		pathList.add(path);
+		//use jsoup library to find image tags
+		File pathFile = path.toFile();
+		Document doc = Jsoup.parse(pathFile, "UTF-8");
+		//String in select() will be a String to identify 
+		//what the method will search for and add to the Elements(list)
+		Elements scripts = doc.getElementsByTag("script");
+		//add each script file found in File to LinkedList<Script> 
+		//in given HTMLDocument Object ignoring inline scripts
+		for (Element s : scripts) {
+			if (s.hasAttr("src")) {
+				Script tempScript = new Script();
+				Path scriptPath = Paths.get(s.attr("src"));
+				tempScript.setPath(scriptPath);
+				scriptList.add(tempScript);
+			}
+		}
+		return scriptList;
 		
 	}
 	
 	/*
 	* 
 	*/
-	public LinkedList<Anchor> scanForAnchors(Path path){
+	public LinkedList<Anchor> scanForAnchors(Path path) throws IOException{
 		return null;
 		
 	}
--- src/test/java/edu/odu/cs/cs350/TestPageReader.java
+++ src/test/java/edu/odu/cs/cs350/TestPageReader.java
@@ -11,6 +11,7 @@ import java.util.LinkedList;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.odu.cs.cs350.Enum.Externality;
 import edu.odu.cs.cs350.Enum.TagType;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -20,29 +21,86 @@ public class TestPageReader {
 	PageReader pgReader = new PageReader();
 	String pathStr1 = "C:\\Users\\chris\\Desktop\\SampleHTML\\page1.html";
 	Path path1 = Paths.get(pathStr1);
+	String pathStr2 = "";
+	Path path2 = Paths.get(pathStr2);
+	String pathStr3 = "";
+	Path path3 = Paths.get(pathStr3);
+	String pathStr4 = "";
+	Path path4 = Paths.get(pathStr4);
 	LinkedList<Image> expectedImages = new LinkedList<Image>();
 	LinkedList<Image> images;
+	LinkedList<Style> expectedStyles = new LinkedList<Style>();
+	LinkedList<Style> styles;
 	LinkedList<Script> expectedScripts = new LinkedList<Script>();
 	LinkedList<Script> scripts;
+	LinkedList<Anchor> expectedAnchors = new LinkedList<Anchor>();
+	LinkedList<Anchor> anchors;
 	
-	@Before
-	public void setup() {
-		LinkedList<Path> paths = new LinkedList<Path>();
-		LinkedList<Path> imgPaths = new LinkedList<Path>();
+	LinkedList<Path> paths = new LinkedList<Path>();
+	LinkedList<Path> imgPaths = new LinkedList<Path>();
+	LinkedList<Path> stylePaths = new LinkedList<Path>();
+	LinkedList<Path> scriptPaths = new LinkedList<Path>();
+	LinkedList<Path> anchorPaths = new LinkedList<Path>();
+	
+	@Test
+	public void testPageReaderImages() throws IOException {
 		paths.add(path1);
 		imgPaths.add(Paths.get("C:\\Users\\chris\\Desktop\\SampleHTML\\Images\\Image1.jpg"));
 		Image img1 = new Image(240698, imgPaths, 1, paths); //235 KB (240,698 bytes)
 		expectedImages.add(img1);
-	}
-	
-	@Test
-	public void testPageReaderOnlyImages() throws IOException {
+		/************************/
 		images = pgReader.scanForImages(path1);
 		LinkedList<Style> styles = pgReader.scanForStyles(path1);
 		scripts = pgReader.scanForScripts(path1);
 		LinkedList<Anchor> anchors = pgReader.scanForAnchors(path1);
-		//System.out.println(page1expected.getImages().get(0).size() + " bytes : " + page1expected.getImages().get(0).listings().toString());
-		//System.out.println(images.get(0).size() + " bytes : " + images.get(0).listings().toString());
 		assertTrue(images.equals(expectedImages));
+		assertTrue(styles.equals(expectedStyles));
+		assertTrue(scripts.equals(expectedScripts));
+		assertTrue(anchors.equals(expectedAnchors));
+	}
+	
+	@Test
+	public void testPageReaderStyles() throws IOException {
+		Path stylePath = Paths.get("");
+		Style style1 = new Style(stylePath,Externality.INTERNAL);
+		/************************/
+		images = pgReader.scanForImages(path2);
+		LinkedList<Style> styles = pgReader.scanForStyles(path2);
+		scripts = pgReader.scanForScripts(path2);
+		LinkedList<Anchor> anchors = pgReader.scanForAnchors(path2);
+		assertTrue(images.equals(expectedImages));
+		assertTrue(styles.equals(expectedStyles));
+		assertTrue(scripts.equals(expectedScripts));
+		assertTrue(anchors.equals(expectedAnchors));
+	}
+	
+	@Test
+	public void testPageReaderScripts() throws IOException {
+		Path scriptPath = Paths.get("");
+		Script script1 = new Script(scriptPath,Externality.INTERNAL);
+		/************************/
+		images = pgReader.scanForImages(path2);
+		LinkedList<Style> styles = pgReader.scanForStyles(path2);
+		scripts = pgReader.scanForScripts(path2);
+		LinkedList<Anchor> anchors = pgReader.scanForAnchors(path2);
+		assertTrue(images.equals(expectedImages));
+		assertTrue(styles.equals(expectedStyles));
+		assertTrue(scripts.equals(expectedScripts));
+		assertTrue(anchors.equals(expectedAnchors));
+	}
+	
+	@Test
+	public void testPageReaderAnchors() throws IOException {
+		Path anchorPath = Paths.get("");
+		Anchor anchor1 = new Anchor(anchorPath,Externality.INTERNAL);
+		/************************/
+		images = pgReader.scanForImages(path2);
+		LinkedList<Style> styles = pgReader.scanForStyles(path2);
+		scripts = pgReader.scanForScripts(path2);
+		LinkedList<Anchor> anchors = pgReader.scanForAnchors(path2);
+		assertTrue(images.equals(expectedImages));
+		assertTrue(styles.equals(expectedStyles));
+		assertTrue(scripts.equals(expectedScripts));
+		assertTrue(anchors.equals(expectedAnchors));
 	}
 }
