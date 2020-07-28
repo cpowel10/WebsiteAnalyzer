41d8a5eaf6e51fda5c4bd456dee391f4924f7a71
rpeabody <rpeabody@cs.odu.edu>
2020-07-27 22:26:23 -0400

    some formatting

--- src/main/java/edu/odu/cs/cs350/Tag.java
+++ src/main/java/edu/odu/cs/cs350/Tag.java
@@ -7,7 +7,6 @@ import edu.odu.cs.cs350.Enum.Externality;
 import edu.odu.cs.cs350.Enum.TagType;
 
 public class Tag {
-
 	private Path path;
 	private TagType type;
 	private Externality eality;
@@ -17,29 +16,37 @@ public class Tag {
 		this.setType(TagType.STYLE); 
 		this.setExternality(Externality.EXTERNAL);
 	}
+
 	public Tag(Path pth, TagType typ, Externality ext) {
 		this.setPath(pth); 
 		this.setType(typ); 
 		this.setExternality(ext);
 		}
-	public Path path() {
+
+	public Path getPath() {
 		return this.path;
 	}
-	public TagType type() {
+
+	public TagType getType() {
 		return this.type;
 	}
-	public Externality externality() {
+
+	public Externality getExternality() {
 		return this.eality;
 	}
+
 	public void setPath(Path path) {
 		this.path = path;
 	}
+
 	public void setType(TagType type) {
 		this.type = type;
 	}
+
 	public void setExternality(Externality ext) {
 		this.eality = ext;
 	}
+
 	@Override
 	public boolean equals(Object other) {
 		if(this == other)
@@ -47,6 +54,6 @@ public class Tag {
 		if(!(other instanceof Tag))
 			return false;
 		Tag otherTag = (Tag) other;
-		return this.path.equals(otherTag.path());
+		return this.path.equals(otherTag.getPath());
 	}
 }
