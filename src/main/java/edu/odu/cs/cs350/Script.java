bb108abda20ce204b46b394d0234816dc4e39785
rpeabody <rpeabody@cs.odu.edu>
2020-07-27 22:52:33 -0400

    working on constructors and st00f

--- src/main/java/edu/odu/cs/cs350/Script.java
+++ src/main/java/edu/odu/cs/cs350/Script.java
@@ -2,16 +2,28 @@ package edu.odu.cs.cs350;
 
 import edu.odu.cs.cs350.Enum.Externality;
 import edu.odu.cs.cs350.Enum.TagType;
+
 import java.nio.file.Path;
+import java.util.LinkedList;
+
+import edu.odu.cs.cs350.Enum.*;
 
 public class Script extends Tag {
+	private LinkedList<Path> listings;
 	public Script() {
-		super();
-		this.setType(TagType.SCRIPT);
+		super(TagType.SCRIPT);
+		listings = new LinkedList<Path>();
 	}
 	
-	public Script(Path inPath, Externality inEality) {
-		super(inPath, TagType.SCRIPT, inEality);
+	public Script(Path myPath, Externality myEality) {
+		super(TagType.SCRIPT, myPath, myEality);
+		listings = new LinkedList<Path>();
+	}
+
+	public Script(Path myPath, Externality myEality, Path firstListing) {
+		super(TagType.SCRIPT, myPath, myEality);
+		listings = new LinkedList<Path>();
+		listings.add(firstListing);
 	}
 
 }
