d3aa2176c4d235bd070d6a248542c4b2b00d863a
rpeabody <rpeabody@cs.odu.edu>
2020-07-27 23:20:40 -0400

    new constructor logic and keeps track of listings

--- src/main/java/edu/odu/cs/cs350/Style.java
+++ src/main/java/edu/odu/cs/cs350/Style.java
@@ -2,15 +2,30 @@ package edu.odu.cs.cs350;
 
 import edu.odu.cs.cs350.Enum.Externality;
 import edu.odu.cs.cs350.Enum.TagType;
+
 import java.nio.file.Path;
+import java.util.LinkedList;
 
 public class Style extends Tag {
+	private LinkedList<Path> listings;
+	
 	public Style() {
-		super();
+		super(TagType.STYLE);
+		listings = new LinkedList<Path>();
 	}
 	
-	public Style(Path inPath, Externality inEality) {
-		super(inPath, TagType.STYLE, inEality);
+	public Style(Path myPath, Externality myEality) {
+		super(TagType.STYLE, myPath, myEality);
+		listings = new LinkedList<Path>();
 	}
 
+	public Style(Path myPath, Externality myEality, Path firstListing) {
+		super(TagType.STYLE, myPath, myEality);
+		listings = new LinkedList<Path>();
+		listings.add(firstListing);
+	}
+
+	public void addListing(Path listing) {
+		listings.add(listing);
+	}
 }
