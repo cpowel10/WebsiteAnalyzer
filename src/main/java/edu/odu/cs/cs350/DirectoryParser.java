c5508a4783257422dd487e6c7c9b2f5c6f03099c
rpeabody <rpeabody@cs.odu.edu>
2020-07-28 21:41:08 -0400

    Begining to categorize files based on extension

--- src/main/java/edu/odu/cs/cs350/DirectoryParser.java
+++ src/main/java/edu/odu/cs/cs350/DirectoryParser.java
@@ -9,7 +9,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import java.net.URL;
-
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,6 +28,13 @@ public class DirectoryParser {
 	private LinkedList<Path> htmlPaths;
 
 	private PathManager pman;
+
+	private List<String> archiveExtensions = 
+		Arrays.asList("zip", "gz", "tar", "7z");
+	private List<String> videoExtensions =
+		Arrays.asList("m4a", "mka", "ogg");
+	private List<String> audioExtensions =
+		Arrays.asList("mkv", "mp4");
 
 	private Path homeDir;
 	private URL[] urls;
@@ -51,13 +58,31 @@ public class DirectoryParser {
 	}
 
 	public void categorizeFiles() {
+		String extension = "";
+		long size = 0;
+
 		for(Path p : foundFiles)
 		{
-			//categorize
+			extension = getExtension(p);
+			if(archiveExtensions.contains(extension)) {
+				foundArchives.add(new ArchiveFile(size, p));
+			}
+			if(videoExtensions.contains(extension)) {
+				foundVideos.add(new VideoFile(size, p));
+			}
+			if(audioExtensions.contains(extension)) {
+				foundAudios.add(new AudioFile(size, p));
+			}
 		}
 	}
 
+	public String getExtension(Path p) {
+		int i = 0;
+		i = p.getFileName().toString().lastIndexOf(".");
+		return p.getFileName().toString().substring(i+1);
+	}
+
 	public List<Path> getPages() {
-		return foundFiles;
+		return htmlPaths;
 	}
 }
