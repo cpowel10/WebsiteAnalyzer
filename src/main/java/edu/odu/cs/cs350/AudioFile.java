c28a92348abbde48b49020ae4580e2cd33508f21
kgrissom <kgris001@odu.edu>
2020-07-30 21:37:38 -0400

    Doc

--- src/main/java/edu/odu/cs/cs350/AudioFile.java
+++ src/main/java/edu/odu/cs/cs350/AudioFile.java
@@ -5,6 +5,11 @@ import java.nio.file.Path;
 import edu.odu.cs.cs350.Enum.FileType;
 
 public class AudioFile extends WebsiteFile {
+	/*
+	 * nondefault contructor
+	 * @param inSize contains long of AudioFile size
+	 * @param inPath contains path to AudioFile
+	 */
 	public AudioFile(long inSize, Path inPath) {
 		super(inSize, inPath, FileType.AUDIO);
 	}
