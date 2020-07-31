73ebce95eadcb17f52206d5ff7e16c3fa41bbfa0
kgrissom <kgris001@odu.edu>
2020-07-30 20:48:20 -0400

    Comments

--- src/main/java/edu/odu/cs/cs350/Style.java
+++ src/main/java/edu/odu/cs/cs350/Style.java
@@ -10,23 +10,32 @@ import java.net.URI;
 
 public class Style extends Tag {
 	private LinkedList<Path> listings;
-	
+	/*
+	 * default constructor
+	 */
 	public Style() {
 		super(TagType.STYLE);
 		listings = new LinkedList<Path>();
 	}
-	
+	/*
+	 * nondefault constructor
+	 */
 	public Style(Path myPath, Externality myEality, URI myUri) {
 		super(TagType.STYLE, myPath, myEality, myUri);
 		listings = new LinkedList<Path>();
 	}
-
+	/*
+	 * second nondefault constructor
+	 */
 	public Style(Path myPath, Externality myEality, Path firstListing, URI myUri) {
 		super(TagType.STYLE, myPath, myEality, myUri);
 		listings = new LinkedList<Path>();
 		listings.add(firstListing);
 	}
-
+	/*
+	 * adds a path to the collection(linked list) of paths to this style
+	 * @param Path to this style
+	 */
 	public void addListing(Path listing) {
 		listings.add(listing);
 	}
