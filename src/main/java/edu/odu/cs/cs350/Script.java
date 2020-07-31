b4df489030ea91fba4d76978591444ffbeb3c2d3
kgrissom <kgris001@odu.edu>
2020-07-30 21:09:38 -0400

    Doc

--- src/main/java/edu/odu/cs/cs350/Script.java
+++ src/main/java/edu/odu/cs/cs350/Script.java
@@ -12,23 +12,39 @@ import edu.odu.cs.cs350.Enum.*;
 
 public class Script extends Tag {
 	private LinkedList<Path> listings;
-	
+	/*
+	 * default constructor
+	 */
 	public Script() {
 		super(TagType.SCRIPT);
 		listings = new LinkedList<Path>();
 	}
-	
+	/*
+	 * nondefault constructor
+	 * @param myPath contains path to script tag
+	 * @param myEality contains externality of script tag
+	 * @param myUri contains URI to script tag
+ 	 */
 	public Script(Path myPath, Externality myEality, URI myUri) {
 		super(TagType.SCRIPT, myPath, myEality, myUri);
 		listings = new LinkedList<Path>();
 	}
-
+	/*
+	 * second nondefault constructor
+	 * @param myPath contains path to script tag
+	 * @param myEality contains externality of script tag
+	 * @param firstListing contains path to be added to listings collection
+	 * @param myUri contains URI to script tag
+ 	 */
 	public Script(Path myPath, Externality myEality, Path firstListing, URI myUri) {
 		super(TagType.SCRIPT, myPath, myEality, myUri);
 		listings = new LinkedList<Path>();
 		listings.add(firstListing);
 	}
-
+	/*
+	 * adds path to collection of paths leading to script tag
+	 * @param listing contains path to be added
+	 */
 	public void addListing(Path listing) {
 		listings.add(listing);
 	}
