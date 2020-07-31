66b4b717365feb8e2fb52011304662b6abcaf1af
kgrissom <kgris001@odu.edu>
2020-07-30 21:21:57 -0400

    Doc

--- src/main/java/edu/odu/cs/cs350/Image.java
+++ src/main/java/edu/odu/cs/cs350/Image.java
@@ -12,49 +12,78 @@ public class Image extends Tag 	implements Comparable<Image> {
 	private long size;
 	private int numPages;
 	private LinkedList<Path> listings;
-	
+	/*
+	 * default constructor
+	 */
 	public Image() {
 		super(TagType.IMAGE);
 		size = 0;
 		listings = new LinkedList<Path>();
 		this.setNumPages(-1);
 	}
-
+	/*
+	 * nondefault constructor
+	 * @param myPath contains a path to the image
+	 * @param mySize contains a long of the image size
+	 * @param firstListing contains path image was first listed on
+	 * @param myExternality contains externality (internal, intrapage, external)
+	 * @param myUri contains URI to image
+	 */
 	public Image(Path myPath, long mySize, Path firstListing, Externality myExternality, URI myUri) {
 		super(TagType.IMAGE, myPath, myExternality, myUri);
 		size = mySize;
 		listings = new LinkedList<Path>();
 		listings.add(firstListing);
 	}
-	
+	/*
+	 * returns the image size
+	 */
 	public long getSize() {
 		return this.size;
 	}
-
+	/*
+	 * returns the linked list of paths that lead to the image
+	 */
 	public LinkedList<Path> getListings() {
 		 return this.listings;
 	}
-
+	/*
+	 * returns the number of pages the image was on
+	 */
 	public int numPages() {
 		return this.numPages;
 	}
-
+	/*
+	 * adds a path to the collection of paths leading to the image
+	 * @param path to image to be added to listings
+	 */
 	public void addListing(Path listing) {
 		listings.add(listing);
 	}
-
+	/*
+	 * increments the number of pages the image is found on
+	 */
 	public void incrementListings() {
 		numPages++;
 	}
-
+	/*
+	 * set the number of pages image is found on
+	 * @param number(integer) to set number of pages to
+	 */
 	public void setNumPages(int num) {
 		numPages = num;
 	}
-
+	/*
+	 * set the size of the image
+	 * @param mySize contains size of image
+	 */
 	public void setSize(long mySize) {
 		size = mySize;
 	}
-
+	/*
+	 * override comparison operator to compare images
+	 * @param image to compare 
+	 */
 	@Override
 	public int compareTo(Image img) {
 		String lhsPath = (this.getPath()).toString();
