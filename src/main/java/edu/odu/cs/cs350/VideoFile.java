f536c3be1a818eb0f58ddd7836daff74f47a926d
kgrissom <kgris001@odu.edu>
2020-07-30 20:51:21 -0400

    Documentation (sounds nicer than comments)

--- src/main/java/edu/odu/cs/cs350/VideoFile.java
+++ src/main/java/edu/odu/cs/cs350/VideoFile.java
@@ -5,6 +5,10 @@ import java.nio.file.Path;
 import edu.odu.cs.cs350.Enum.FileType;
 
 public class VideoFile extends WebsiteFile {
+	/*
+	 * nondefault constructor,
+	 * no default constructor because videoFiles should not exist without a size and path
+	 */
 	public VideoFile(long inSize, Path inPath) {
 		super(inSize, inPath, FileType.VIDEO);
 	}
