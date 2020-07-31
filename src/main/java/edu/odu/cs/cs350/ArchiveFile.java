96e177132eccbd1b40c42d74c3b61035da399d7e
kgrissom <kgris001@odu.edu>
2020-07-30 21:38:47 -0400

    Doc

--- src/main/java/edu/odu/cs/cs350/ArchiveFile.java
+++ src/main/java/edu/odu/cs/cs350/ArchiveFile.java
@@ -5,6 +5,11 @@ import java.nio.file.Path;
 import edu.odu.cs.cs350.Enum.FileType;
 
 public class ArchiveFile extends WebsiteFile {
+	/*
+	 * nondefault constructor
+	 * @param inSize contains size of ArchiveFile
+	 * @param inPath contains path to ArchiveFile
+	 */
 	public ArchiveFile(long inSize, Path inPath) {
 		super(inSize, inPath, FileType.ARCHIVE);
 	}
