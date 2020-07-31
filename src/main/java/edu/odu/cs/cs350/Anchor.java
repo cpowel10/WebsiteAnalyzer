8d385a63351398468d563bdffaed1d2e2052f166
kgrissom <kgris001@odu.edu>
2020-07-30 21:40:53 -0400

    Doc

--- src/main/java/edu/odu/cs/cs350/Anchor.java
+++ src/main/java/edu/odu/cs/cs350/Anchor.java
@@ -8,10 +8,18 @@ import java.net.URI;
 import java.nio.file.Path;
 
 public class Anchor extends Tag {
+	/*
+	 * default contructor
+	 */
 	public Anchor() {
 		super(TagType.ANCHOR);
 	}
-	
+	/*
+	 * nondefault constructor
+	 * @param myPath contains path to anchor tag
+	 * @param myEality contains externality of tag (internal, intrapage, external)
+	 * @param myUri contains Uri to anchor tag
+	 */	
 	public Anchor(Path myPath, Externality myEality, URI myUri) {
 		super(TagType.ANCHOR, myPath, myEality, myUri);
 	}
