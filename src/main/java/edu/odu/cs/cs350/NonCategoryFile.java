6de775a087de4decd36710811c8ec97ddd34eee0
kgrissom <kgris001@odu.edu>
2020-07-30 21:14:32 -0400

    Doc

--- src/main/java/edu/odu/cs/cs350/NonCategoryFile.java
+++ src/main/java/edu/odu/cs/cs350/NonCategoryFile.java
@@ -5,6 +5,11 @@ import java.nio.file.Path;
 import edu.odu.cs.cs350.Enum.FileType;
 
 public class NonCategoryFile extends WebsiteFile {	
+	/*
+	 * nondefault constructor
+	 * @param inSize contains a long of the size of the non-category file
+	 * @param inPath contains path to non-category file
+	 */
 	public NonCategoryFile(long inSize, Path inPath) {
 		super(inSize, inPath, FileType.NONCATEGORY);
 	}
