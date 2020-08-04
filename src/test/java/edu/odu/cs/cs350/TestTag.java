30edc4a92ffe940d84094a5ad79d2b7eb56d083e
kgrissom <kgris001@odu.edu>
2020-08-04 15:15:43 -0400

    Made compile, Nick and Grayson can complete tests

--- src/test/java/edu/odu/cs/cs350/TestTag.java
+++ src/test/java/edu/odu/cs/cs350/TestTag.java
@@ -5,6 +5,7 @@ import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.CoreMatchers.*;
 import java.util.LinkedList;
 import java.nio.file.Paths;
+import java.net.URI;
 import java.nio.file.Path;
 import org.junit.Before;
 import org.hamcrest.core.IsNull;
@@ -40,39 +41,39 @@ public class TestTag {
 	Path pth = Paths.get("my/original/testing/path");
 	TagType typ = TagType.ANCHOR;
 	Externality ext = Externality.INTRA;
-	Tag myTag = new Tag(pth, typ, ext);
+	Tag myTag = new Tag(typ, pth, ext, URI.create("fakeURI"));
 	
 	
 	@Test
 	public final void testDefaultConstructor(){
-	assertThat(firstTag.path(), is(Paths.get("noAssignedPath")));
-	assertThat(firstTag.type(), is(TagType.STYLE));
-	assertThat(firstTag.externality(), is(Externality.EXTERNAL));
+	assertThat(firstTag.getPath(), is(Paths.get("noAssignedPath")));
+	assertThat(firstTag.getType(), is(TagType.STYLE));
+	assertThat(firstTag.getExternality(), is(Externality.EXTERNAL));
 	}
 		
 	@Test
 	public final void testNonDefaultContructor(){
-	assertThat(myTag.path(), is(pth));
-	assertThat(myTag.type(), is(typ));
-	assertThat(myTag.externality(), is(ext));
+	assertThat(myTag.getPath(), is(pth));
+	assertThat(myTag.getType(), is(typ));
+	assertThat(myTag.getExternality(), is(ext));
 	}
 		
 	@Test
 	public final void testSetPath(){
 	Path newPath = Paths.get("my/new/testing/path");
 	myTag.setPath(newPath);
-	assertThat(myTag.path(), is(newPath));
-	assertThat(myTag.type(), is(typ));
-	assertThat(myTag.externality(), is(ext));
+	assertThat(myTag.getPath(), is(newPath));
+	assertThat(myTag.getType(), is(typ));
+	assertThat(myTag.getExternality(), is(ext));
 	}
 		
 	@Test
 	public final void testSetType(){
 	TagType newType = TagType.SCRIPT;
 	myTag.setType(newType);
-	assertThat(myTag.path(), is(pth));
-	assertThat(myTag.type(), is(newType));
-	assertThat(myTag.externality(), is(ext));	
+	assertThat(myTag.getPath(), is(pth));
+	assertThat(myTag.getType(), is(newType));
+	assertThat(myTag.getExternality(), is(ext));	
 	}
 	
 		
@@ -80,8 +81,8 @@ public class TestTag {
 	public final void testExternality(){
 	Externality newExt = Externality.INTERNAL;
 	myTag.setExternality(newExt);
-	assertThat(myTag.path(), is(pth));
-	assertThat(myTag.type(), is(typ));
-	assertThat(myTag.externality(), is(newExt));
+	assertThat(myTag.getPath(), is(pth));
+	assertThat(myTag.getType(), is(typ));
+	assertThat(myTag.getExternality(), is(newExt));
 	}
 }
