cfbc57f18ed78926f4a75bf7d3593c7a94a5dc92
kgrissom <kgris001@odu.edu>
2020-08-06 19:55:37 -0400

    dinner break

--- src/test/java/edu/odu/cs/cs350/TestHTMLDocument.java
+++ src/test/java/edu/odu/cs/cs350/TestHTMLDocument.java
@@ -1,23 +1,20 @@
 package edu.odu.cs.cs350;
 
-import static org.junit.Assert.*;
-
-import static org.junit.Assert.assertThat;
-
-import java.nio.file.Path;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
 import java.nio.file.Paths;
-import java.util.LinkedList;
-import java.net.URI;
+import java.nio.file.Path;
 import org.junit.Test;
+import java.net.URI;
 
 import edu.odu.cs.cs350.Enum.Externality;
 import edu.odu.cs.cs350.Enum.TagType;
+import java.util.LinkedList;
 
-import static org.hamcrest.CoreMatchers.*;
 
 public class TestHTMLDocument {
 
-	String fileName = "/c/Users/chris/Downloads/";
+/*	String fileName = "/c/Users/chris/Downloads/";
 	Path path1 = Paths.get(fileName);
 	LinkedList<Path> llp = new LinkedList<Path>();
 	LinkedList<Path> llp2 = new LinkedList<Path>();
@@ -35,87 +32,114 @@ public class TestHTMLDocument {
 	Style sty2 = new Style(test2, Externality.EXTERNAL, URI.create("fakeURI"));
 	Script script1 = new Script(test1, Externality.INTRA, URI.create("fakeURI"));
 	Anchor anch1 = new Anchor(test2, Externality.INTERNAL, URI.create("fakeURI"));
-	HTMLDocument page1 = new HTMLDocument(path1);
+	//HTMLDocument doc = new HTMLDocument(path1);
+	*/
+	
 
 	@Test
-	public void testConstructorEmpty() {
-		assertThat(page1.getPath(), is(path1));
-		assertThat(page1.getImages(), is(images));
-		assertThat(page1.getStyles(), is(styles));
-		assertThat(page1.getScripts(), is(scripts));
-		assertThat(page1.getAnchors(), is(anchors));
-		assertThat(page1.toString(), is("\\c\\Users\\chris\\Downloads 0 0 0 0 0"));
+	public void testDefaultConstructor() {
+		Path path = Paths.get("pathNotSet");
+		LinkedList<Image> images = new LinkedList<Image>();
+		LinkedList<Style> styles = new LinkedList<Style>();
+		LinkedList<Script> scripts = new LinkedList<Script>();
+		LinkedList<Anchor> anchors = new LinkedList<Anchor>();
+		long size = 0;
+		HTMLDocument doc = new HTMLDocument();
+
+		assertThat(doc.getPath(), is(path));
+		assertThat(doc.getImages(), is(images));
+		assertThat(doc.getStyles(), is(styles));
+		assertThat(doc.getScripts(), is(scripts));
+		assertThat(doc.getAnchors(), is(anchors));
+		assertThat(doc.getTotalImageSize(), is(size));
 	}
-	
+	public void testNonDefaultConstructor() {
+		Path path = Paths.get("pathSet");
+		LinkedList<Image> images = new LinkedList<Image>();
+		LinkedList<Style> styles = new LinkedList<Style>();
+		LinkedList<Script> scripts = new LinkedList<Script>();
+		LinkedList<Anchor> anchors = new LinkedList<Anchor>();
+		long size = 0;
+		HTMLDocument doc = new HTMLDocument(path);
+
+		assertThat(doc.getPath(), is(path));
+		assertThat(doc.getImages(), is(images));
+		assertThat(doc.getStyles(), is(styles));
+		assertThat(doc.getScripts(), is(scripts));
+		assertThat(doc.getAnchors(), is(anchors));
+		assertThat(doc.getTotalImageSize(), is(size));
+	}
+	/*
 	@Test
 	public void testAddImage() {
-		page1.addElement(img1);
+		doc.addElement(img1);
 		images.add(img1);
-		assertThat(page1.getPath(), is(path1));
-		assertThat(page1.getImages(), is(images));
-		assertThat(page1.getStyles(), is(styles));
-		assertThat(page1.getScripts(), is(scripts));
-		assertThat(page1.getAnchors(), is(anchors));
-		assertThat(page1.toString(), is("\\c\\Users\\chris\\Downloads 1 0 0 0 1"));
+		assertThat(doc.getPath(), is(path1));
+		assertThat(doc.getImages(), is(images));
+		assertThat(doc.getStyles(), is(styles));
+		assertThat(doc.getScripts(), is(scripts));
+		assertThat(doc.getAnchors(), is(anchors));
+		assertThat(doc.toString(), is("\\c\\Users\\chris\\Downloads 1 0 0 0 1"));
 	}
 	
 	@Test
 	public void testAddScript() {
-		page1.addElement(script1);
+		doc.addElement(script1);
 		scripts.add(script1);
-		assertThat(page1.getPath(), is(path1));
-		assertThat(page1.getImages(), is(images));
-		assertThat(page1.getStyles(), is(styles));
-		assertThat(page1.getScripts(), is(scripts));
-		assertThat(page1.getAnchors(), is(anchors));
-		assertThat(page1.toString(), is("\\c\\Users\\chris\\Downloads 0 0 1 0 0"));
+		assertThat(doc.getPath(), is(path1));
+		assertThat(doc.getImages(), is(images));
+		assertThat(doc.getStyles(), is(styles));
+		assertThat(doc.getScripts(), is(scripts));
+		assertThat(doc.getAnchors(), is(anchors));
+		assertThat(doc.toString(), is("\\c\\Users\\chris\\Downloads 0 0 1 0 0"));
 	}
 	
 	@Test
 	public void testAddStyle() {
-		page1.addElement(sty1);
+		doc.addElement(sty1);
 		styles.add(sty1);
-		assertThat(page1.getPath(), is(path1));
-		assertThat(page1.getImages(), is(images));
-		assertThat(page1.getStyles(), is(styles));
-		assertThat(page1.getScripts(), is(scripts));
-		assertThat(page1.getAnchors(), is(anchors));
-		assertThat(page1.toString(), is("\\c\\Users\\chris\\Downloads 0 1 0 0 0"));
+		assertThat(doc.getPath(), is(path1));
+		assertThat(doc.getImages(), is(images));
+		assertThat(doc.getStyles(), is(styles));
+		assertThat(doc.getScripts(), is(scripts));
+		assertThat(doc.getAnchors(), is(anchors));
+		assertThat(doc.toString(), is("\\c\\Users\\chris\\Downloads 0 1 0 0 0"));
 	}
 
 	@Test
 	public void testAddAnchor() {
-		page1.addElement(anch1);
+		doc.addElement(anch1);
 		anchors.add(anch1);
-		assertThat(page1.getPath(), is(path1));
-		assertThat(page1.getImages(), is(images));
-		assertThat(page1.getStyles(), is(styles));
-		assertThat(page1.getScripts(), is(scripts));
-		assertThat(page1.getAnchors(), is(anchors));
-		assertThat(page1.toString(), is("\\c\\Users\\chris\\Downloads 0 0 0 1 0"));
+		assertThat(doc.getPath(), is(path1));
+		assertThat(doc.getImages(), is(images));
+		assertThat(doc.getStyles(), is(styles));
+		assertThat(doc.getScripts(), is(scripts));
+		assertThat(doc.getAnchors(), is(anchors));
+		assertThat(doc.toString(), is("\\c\\Users\\chris\\Downloads 0 0 0 1 0"));
 	}
 	
 	@Test
 	public void testAddAll() {
-		page1.addElement(img1);
-		page1.addElement(img2);
-		page1.addElement(img3);
+		doc.addElement(img1);
+		doc.addElement(img2);
+		doc.addElement(img3);
 		images.add(img1);
 		images.add(img2);
 		images.add(img3);
-		page1.addElement(script1);
+		doc.addElement(script1);
 		scripts.add(script1);
-		page1.addElement(sty1);
-		page1.addElement(sty2);
+		doc.addElement(sty1);
+		doc.addElement(sty2);
 		styles.add(sty1);
 		styles.add(sty2);
-		page1.addElement(anch1);
+		doc.addElement(anch1);
 		anchors.add(anch1);
-		assertThat(page1.getPath(), is(path1));
-		assertThat(page1.getImages(), is(images));
-		assertThat(page1.getStyles(), is(styles));
-		assertThat(page1.getScripts(), is(scripts));
-		assertThat(page1.getAnchors(), is(anchors));
-		assertThat(page1.toString(), is("\\c\\Users\\chris\\Downloads 3 2 1 1 6"));
+		assertThat(doc.getPath(), is(path1));
+		assertThat(doc.getImages(), is(images));
+		assertThat(doc.getStyles(), is(styles));
+		assertThat(doc.getScripts(), is(scripts));
+		assertThat(doc.getAnchors(), is(anchors));
+		assertThat(doc.toString(), is("\\c\\Users\\chris\\Downloads 3 2 1 1 6"));
 	}
+	*/
 }
