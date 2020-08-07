a5577c94574f7f178322a7e93e8ffcb9f404326e
kgrissom <kgris001@odu.edu>
2020-08-06 21:17:12 -0400

    /** not /*

--- src/main/java/edu/odu/cs/cs350/Tag.java
+++ src/main/java/edu/odu/cs/cs350/Tag.java
@@ -2,10 +2,7 @@ package edu.odu.cs.cs350;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import java.net.URI;
-//import java.net.URL;
-
 import edu.odu.cs.cs350.Enum.Externality;
 import edu.odu.cs.cs350.Enum.TagType;
 
@@ -14,7 +11,7 @@ public class Tag {
 	private URI uri;
 	private TagType type;
 	private Externality eality;
-	/*
+	/**
 	 * default constructor
 	 */
 	public Tag() {
@@ -22,7 +19,7 @@ public class Tag {
 		type = TagType.UNDEFINED; 
 		eality = Externality.UNDEFINED;
 	}
-	/*
+	/**
 	 * nondefault constructor
 	 * @param myType is the tag type (anchor, script, etc) (Enumerated type not string!)
 	 */
@@ -31,7 +28,7 @@ public class Tag {
 		type = mytype; 
 		eality = Externality.UNDEFINED;
 	}
-	/*
+	/**
 	 * second nondefault constructor
 	 * @param myType is the tag type (anchor, script, etc) (Enumerated type not string!)
 	 * @param myPath is the path to such tag (of type path)
@@ -44,7 +41,7 @@ public class Tag {
 		eality = myEality;
 		uri = myUri;
 	}
-	/*
+	/**
 	 * nondefault constructor that PageReader uses to make Raw tags
 	 * @param myUri is the URI of the tag
 	 */
@@ -54,52 +51,52 @@ public class Tag {
 		eality = Externality.UNDEFINED;
 		uri = myUri;
 	}
-	/*
+	/**
 	 * returns path of tag
 	 */
 	public Path getPath() {
 		return this.path;
 	}
-	/*
+	/**
 	 * returns Tag type of tag
 	 */
 	public TagType getType() {
 		return this.type;
 	}
-	/*
+	/**
 	 * returns externality of tag
 	 */
 	public Externality getExternality() {
 		return this.eality;
 	}
-	/*
+	/**
 	 * returns URI of tag
 	 */
 	public URI getUri() {
 		return this.uri;
 	}
-	/*
+	/**
 	 * sets path of tag
 	 * @param path to tag
 	 */
 	public void setPath(Path path) {
 		this.path = path;
 	}
-	/*
+	/**
 	 * sets tag type of tag
 	 * @param tag type of tag
 	 */
 	public void setType(TagType type) {
 		this.type = type;
 	}
-	/*
+	/**
 	 * sets externality of tag
 	 * @param externality of tag
 	 */
 	public void setExternality(Externality ext) {
 		this.eality = ext;
 	}
-	/*
+	/**
 	 * overide equals to compare tag equality
 	 * @param Object to be compared for equality
 	 */
